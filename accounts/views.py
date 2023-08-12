2a4c24d5c64969fb3eaadfb1343500561975ef53
Mangrove-Sun <tjsghd0317@gmail.com>
2023-08-12 14:57:02 +0900

    로그인/로그아웃 구현 그리고 회원 가입과 동시에 로그인

--- accounts/views.py
+++ accounts/views.py
@@ -1,13 +1,22 @@
 from django.contrib import messages
+from django.contrib.auth import login as auth_login
+from django.contrib.auth.views import LoginView, logout_then_login
 from django.shortcuts import redirect, render
 from .forms import SignupForm
 
+
+login = LoginView.as_view(template_name = 'accounts/login_form.html')
+
+def logout(request):
+  messages.success(request, '로그아웃되었습니다.')
+  return logout_then_login(request)
 
 def signup(request):
   if request.method == "POST":
     form = SignupForm(request.POST)
     if form.is_valid():
       signed_user = form.save()
+      auth_login(request, signed_user) # 회원가입 후 바로 로그인되는 기능 login as auth_login
       messages.success(request, "회원가입 환영합니다.")
       signed_user.send_welcome_email() # FIXME: Celey로 처리하는 것을 추천.
       # request에서 GET인자에서 'next'가 없으면 '/'로 설정을 하겠다.
@@ -18,3 +27,4 @@ def signup(request):
   return render(request, "accounts/signup_form.html", {
     'form': form,
   })
+  
