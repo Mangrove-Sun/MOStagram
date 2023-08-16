37c58dfc358f4fdf01f465cbae11f30e5e514276
Mangrove-Sun <tjsghd0317@gmail.com>
2023-08-16 18:15:14 +0900

    유저페이지에서 Unfollow 구현

--- instagram/views.py
+++ instagram/views.py
@@ -46,8 +46,14 @@ def user_page(request, username):
   post_list = Post.objects.filter(author = page_user)
   post_list_count = post_list.count() # 실제 데이터베이스에 count 쿼리를 던지게 된다. post개수가 많아지게 되면 속도에서 차이가 날 수 있다.
   # len(post_list)  # post_list를 전체를 다 가져와서 메모리에 얹어서 메모리상의 리스트 개수를 반환
+  
+  if request.user.is_authenticated:
+    is_follow = request.user.following_set.filter(pk = page_user.pk).exists() # login되어있으면 User객체, 안 되어있으면 AnonymousUser
+  else:
+    is_follow = False
   return render(request, "instagram/user_page.html", {
     "page_user": page_user,
     "post_list": post_list,
     "post_list_count": post_list_count,
+    "is_follow": is_follow,
     })
