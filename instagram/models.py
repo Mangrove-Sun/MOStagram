ff435471837cc759d009ada4fe54560cd1ff1bea
Mangrove-Sun <tjsghd0317@gmail.com>
2023-08-16 19:26:53 +0900

    포스팅 좋아요/취소 구현

--- instagram/models.py
+++ instagram/models.py
@@ -11,12 +11,16 @@ class BaseModel(models.Model):
   class Meta:
     abstract = True
     
+# user
+# -> Post.objects.filter(author=user)
+# -> user.post_set.all()
 class Post(BaseModel):
-  author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete = models.CASCADE) # 외래키이기 때문에 on_delete-models.CASCADE 적용
+  author = models.ForeignKey(settings.AUTH_USER_MODEL, related_name = "my_post_set", on_delete = models.CASCADE) # 외래키이기 때문에 on_delete-models.CASCADE 적용
   photo = models.ImageField(upload_to = "instagram/post/%Y/%m/%d")
   caption = models.TextField(max_length = 500)
   tag_set = models.ManyToManyField("Tag", blank = True)
   location = models.CharField(max_length = 100)
+  like_user_set = models.ManyToManyField(settings.AUTH_USER_MODEL, related_name = "like_post_set", blank = True) # !post와 user간의 MTM관계를 이렇게 만드는 방법 1
   
   def __str__(self):
     return self.caption
@@ -28,10 +32,15 @@ class Post(BaseModel):
       tag, _ = Tag.objects.get_or_create(name=tag_name)
       tag_list.append(tag)
     return tag_list
-
   
   def get_absolute_url(self):
       return reverse("instagram:post_detail", args=[self.pk])
+    
+  def is_like_user(self, user):
+    return self.like_user_set.filter(pk = user.pk).exists()
+  
+  class Meta:
+    ordering = ['-id']
   
   
 class Tag(models.Model):
@@ -39,3 +48,8 @@ class Tag(models.Model):
   
   def __str__(self):
     return self.name
+  
+  # !방법 2
+# class LikeUser(models.Model):
+#   post = models.ForeignKey(Post, on_delete = models.CASCADE)
+#   user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete = models.CASCADE)
--- instagram/urls.py
+++ instagram/urls.py
@@ -7,6 +7,8 @@ urlpatterns = [
   path('', views.index, name = 'index'),
   path('post/new/', views.post_new, name = "post_new"),
   path('post/<int:pk>/', views.post_detail, name = "post_detail"),
+  path('post/<int:pk>/like/', views.post_like, name = "post_like"),
+  path('post/<int:pk>/unlike/', views.post_unlike, name = "post_unlike"),
   # re_path에서 정규표현식 사용시 시작(')과 끝(') 사이에 $가 반드시 있어야 한다.
   # 그렇지 않으면 정의한 정규표현식 패턴대로 끝나지 않아도 항상 정의한 패턴에 포함이 된다.
   re_path(r'^(?P<username>[\w.@+-]+)/$', views.user_page, name = "user_page"),
