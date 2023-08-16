84bf4557db0fb2f4154d9f07c6a632e9cad1cd4b
Mangrove-Sun <tjsghd0317@gmail.com>
2023-08-16 15:52:10 +0900

    유저 페이지 구현

--- instagram/urls.py
+++ instagram/urls.py
@@ -1,9 +1,12 @@
+from django.urls import path, re_path
 from . import views
-from django.urls import path
 
 app_name = "instagram"
 
 urlpatterns = [
     path('post/new/', views.post_new, name = "post_new"),
     path('post/<int:pk>/', views.post_detail, name = "post_detail"),
+    # re_path에서 정규표현식 사용시 시작(')과 끝(') 사이에 $가 반드시 있어야 한다.
+    # 그렇지 않으면 정의한 정규표현식 패턴대로 끝나지 않아도 항상 정의한 패턴에 포함이 된다.
+    re_path(r'^(?P<username>[\w.@+-]+)/$', views.user_page, name = "user_page"),
 ]
